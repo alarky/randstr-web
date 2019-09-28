d1ffaee0f064152d3a627d5080f3cd9c330ec851
alarky <alarky@gmail.com>
2019-09-29 01:16:07 +0900

    build setting

---
+++ www/precache-manifest.334422eb42cdc24e927ab036b93e34e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c0fc023460dc61016b2671d7fcbc73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "033efbf077e76a3bc5fa",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "25baad70a7f618cb93b4",
+    "url": "/static/js/2.c583d236.chunk.js"
+  },
+  {
+    "revision": "033efbf077e76a3bc5fa",
+    "url": "/static/js/main.c6c69be9.chunk.js"
+  },
+  {
+    "revision": "1e942fb68b0224b10d30",
+    "url": "/static/js/runtime-main.1a8ec931.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
